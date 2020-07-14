83c3ced2cf2abd954805280506acea0785582f1b
Monssaf Toukal <tmonssaf@gmail.com>
2020-07-14 16:32:20 -0400

    run_benchmarks in script

--- krylov_CI.jl
+++ krylov_CI.jl
@@ -8,10 +8,37 @@ Pkg.update()
 
 using PkgBenchmark
 using SuiteSparseMatrixCollection
-
+using JSON
 
 ufl_posdef = filter(p -> p.structure == "symmetric" && p.posDef == "yes" && p.type == "real" && p.rows ≤ 100, ssmc)
-# fetch_ssmc(ufl_posdef, format="MM")
+fetch_ssmc(ufl_posdef, format="MM")
 
-PkgBenchmark.benchmarkpkg("Krylov", "master")
-# include("./benchmark/run_benchmarks.jl")
+commit = benchmarkpkg("Krylov")
+master = benchmarkpkg("Krylov", "master")
+judgement = judge(commit, master)
+export_markdown("judgement.md", judgement)
+export_markdown("master.md", master)
+export_markdown("commit.md", commit)
+
+gist_json = JSON.parse("""
+    {
+        "description": "A benchmark for Krylov repository",
+        "public": true,
+        "files": {
+            "judgement.md": {
+                "content": "$(escape_string(sprint(export_markdown, judgement)))"
+            },
+            "master.md": {
+                "content": "$(escape_string(sprint(export_markdown, master)))"
+            },
+            "commit.md": {
+                "content": "$(escape_string(sprint(export_markdown, commit)))"
+            }
+        }
+    }""")
+
+# Need to add GITHUB_AUTH to your .bashrc
+myauth = GitHub.authenticate(ENV["GITHUB_AUTH"])
+posted_gist = create_gist(params = gist_json, auth = myauth)
+println(posted_gist.html_url)
+
