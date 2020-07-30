44b9ee08be017b1591f2f59dd61b880570ea9134
Monssaf Toukal <tmonssaf@gmail.com>
2020-07-30 11:51:23 -0400

    debigging

--- benchmark/krylov_CI.jl
+++ benchmark/krylov_CI.jl
@@ -1,9 +1,12 @@
 using Pkg
 Pkg.activate("./benchmark/")
+print("load path 1: $(Base.load_path())")
+
 
 Pkg.pin(PackageSpec(name="LearnBase", version="0.3"))
 Pkg.develop(PackageSpec(path="."))
 Pkg.update()
+print("load path 2: $(Base.load_path())")
 
 using SuiteSparseMatrixCollection
 
